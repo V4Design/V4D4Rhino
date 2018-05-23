1c3ae0148b23ded400cf6d7d95578d5564b4663e
Luis Fraguada <luis@mcneel.com>
2018-05-23 10:58:24 +0200

    Adding basic Rhino Plugin boilerplate, including NuGet Package References and a few other edits to the csproj for proper references and build targets.

---
+++ V4D4RhinoPlugin/V4D4RhinoCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace V4D4Rhino
+{
+    public class V4D4RhinoCommand : Command
+    {
+        public V4D4RhinoCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static V4D4RhinoCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "V4D4RhinoCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("The {0} command is under construction.", EnglishName);
+
+            return Result.Success;
+        }
+    }
+}

dc536cf08c867a293844d80ce8b78fd8e45d3b86
Luis Fraguada <luis@mcneel.com>
2018-10-09 07:03:33 +0200

    Adding new Viewer project. TODO: Cleanout other view directories

---
+++ V4D4RhinoViewer/v4d4rhinoviewer/src/components/ElementCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-card>
+        <ElementDetails :title="title" thumb="placeholder.png" :description="description" :visible="showDialog" @close="showDialog=false"/>
+        <v-img :src="thumb" @click.stop="showDialog=true" style="cursor:pointer;"></v-img>
+        <v-card-title primary-title>
+          <div>
+            <h4 >{{ title}}</h4>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>favorite</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>bookmark</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>share</v-icon>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+import ElementDetails from './ElementDetails.vue'
+export default {
+  name: 'ResultElement',
+  components: {
+    ElementDetails
+  },
+  props: {
+    title: String,
+    description: String,
+    thumb: String,
+    flex: Number
+  },
+  data: () => ({
+    show: false,
+    showDialog: false
+  }),
+  methods: {
+    clickedImg () {}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ V4D4RhinoViewer/v4d4rhinoviewer/src/components/QueryForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div><Query/></div>
+        <div><QueryResults/></div>
+    </div>
+</template>
+
+<script>
+import Query from './Query.vue'
+import QueryResults from './QueryResults.vue'
+export default {
+  name: 'QueryForm',
+  components: {
+    Query,
+    QueryResults
+  }
+}
+</script>
+
+<style>
+
+</style>
